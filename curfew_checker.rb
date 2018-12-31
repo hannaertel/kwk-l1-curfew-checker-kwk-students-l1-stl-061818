a3c441768f5ac72218cc37c266a69b7caeb36811
hannaertel <hannaertel@yahoo.com>
2018-12-31 17:17:36 +0000

    Automatically backed up by Learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,11 +1,13 @@
 
 
-curfew = Time>23 && Time<6
+
 if Time.now.hour<23 && Time.now.hour>=6
   puts "You are fine"
 else
   puts "You're in trouble"
 end
+
+
 
 if Time.now.hour == curfew
   puts "oh no"
