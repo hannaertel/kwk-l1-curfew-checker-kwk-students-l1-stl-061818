c34f44d4d31e4fa421485232bb25b7ac3a7d8f65
hannaertel <hannaertel@yahoo.com>
2018-12-31 17:09:41 +0000

    Automatically backed up by Learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,10 +1,13 @@
 
-
-
+class curfews
+curfew>23 && curfew<6
 if Time.now.hour<23 && Time.now.hour>=6
   puts "You are fine"
 else
   puts "You're in trouble"
 end
 
-if Time.now.hour
+if Time.now.hour == curfew
+  puts "oh no"
+else
+  puts "You have #{"
